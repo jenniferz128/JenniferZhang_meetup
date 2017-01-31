e72ca5925f55161682bee03bb75ac0040f0ded26
JenniferZhang128 <jessie.ying@gmail.com>
2017-01-31 13:22:12 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JenniferZhangMeetup::Application.config.secret_key_base = 'd620224c72506c8e27b8c83c1e75fd602784ec5ceb7297844769fc4040d4e6464828ba242bb8cf96450e24ca0bcf02978235567eba0e4a06a5013a22a93df9f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JenniferZhangMeetup::Application.config.session_store :cookie_store, key: '_JenniferZhang_meetup_session'
